2aaae6ff775fa34de7030e2ce9eeba61d8d54d14
Antoine-LB <antoine.stc25@gmail.com>
2022-08-19 15:38:44 +0200

    more images png

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const sharp = require("sharp");
 
-sharp("img/jpeg/dog_1.jpeg").toFile("output.avif", (err, info) => {
-  console.log("err", err);
-  console.log("info", info);
+const FOLDER_PATH = "img/HEIC/"; // with last slash
+const FILE_NAME = "IMG_6327";
+const FILE_EXTENTION = ".HEIC";
+
+const OUTPUT_PATH = "output/"; // with last slash
+
+const EXTENTIONS = [
+  ".jpeg",
+  ".jpg",
+  ".png",
+  ".webp",
+  ".gif",
+  ".bmp",
+  ".tiff",
+  ".svg",
+  ".ico",
+];
+
+EXTENTIONS.forEach((extention) => {
+  console.log("=== ", extention, " ====");
+  sharp(`${FOLDER_PATH}${FILE_NAME}${FILE_EXTENTION}`).toFile(
+    `${FILE_NAME}${extention}`,
+    (err, info) => {
+      console.log("err", err);
+      console.log("info", info);
+    }
+  );
+  console.log("======");
 });
